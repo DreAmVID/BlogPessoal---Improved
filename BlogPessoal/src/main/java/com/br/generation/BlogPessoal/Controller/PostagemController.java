c27c0c8a8d72cb7e5de77537b986159494db6dd3
David <daviidccastro@gmail.com>
2022-02-04 22:59:25 -0300

    @DreAmVID [Criado método POST na Postagem Controller]

--- BlogPessoal/src/main/java/com/br/generation/BlogPessoal/Controller/PostagemController.java
+++ BlogPessoal/src/main/java/com/br/generation/BlogPessoal/Controller/PostagemController.java
@@ -3,10 +3,13 @@ package com.br.generation.BlogPessoal.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -65,5 +68,28 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) {
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
+	
+	/**
+	 * Método para enviar através da body uma postagem.
+	 * @author @DreAmVID
+	 * @since 1.0
+	 * @date 04-02-2022
+	 *
+	 */
+	
+	@PostMapping
+	public ResponseEntity<Postagem> CriarUmPost(@RequestBody Postagem conteudo) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(conteudo));	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
