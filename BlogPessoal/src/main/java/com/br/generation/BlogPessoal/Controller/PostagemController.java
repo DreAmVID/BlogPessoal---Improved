4400ece8b9367151f64c39ca02172fb3181c119c
David <daviidccastro@gmail.com>
2022-02-04 22:52:10 -0300

    @DreAmVID [Adiciona métodos GetByID e GetAllByTitulo em Postagem Controller]

--- BlogPessoal/src/main/java/com/br/generation/BlogPessoal/Controller/PostagemController.java
+++ BlogPessoal/src/main/java/com/br/generation/BlogPessoal/Controller/PostagemController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +21,49 @@ public class PostagemController {
 	@Autowired
 	private PostagemRepository repository;
 	
-	public ResponseEntity<List<Postagem>> GetAll(){
-		return ResponseEntity.ok(repository.findAll());
+	/**
+	 * Método para retornar na body todos as postagens cadastradas 
+	 * @author @DreAmVID
+	 * @since 1.0
+	 * @date 04-02-2022
+	 *
+	 */
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll()	{
+		return ResponseEntity.ok(repository.findAll());	
 	}
-
+	
+	/**
+	 * Método para retornar na body as postagens através de um filtro de ID
+	 * utilizado condição λ para retornar em condições de requisições nulas ou incorretas
+	 * e em caso de solicitações que atendem ao método.
+	 * @author @DreAmVID
+	 * @since 1.0
+	 * @date 04-02-2022
+	 *
+	 */
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> GetById(@PathVariable long id) {
+			return repository.findById(id)
+					.map(resp -> ResponseEntity.ok(resp))
+					.orElse(ResponseEntity.notFound().build());
+	}
+	
+	/**
+	 * Método para retornar na body as postagens através de um filtro de titulo
+	 * note que neste caso é possível que exista mais de uma postagem com o mesmo titulo
+	 * logo a resposta deve ser retornada atráves de uma lista
+	 * @author @DreAmVID
+	 * @since 1.0
+	 * @date 04-02-2022
+	 *
+	 */
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
 }
