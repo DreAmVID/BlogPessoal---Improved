a72049e6dd7523b35204ad12bc8748f4f4355194
David <daviidccastro@gmail.com>
2022-02-05 02:38:14 -0300

    @DreAmVID [Criado métodos POST, PUT e DELETE no TemaController]

--- BlogPessoal/src/main/java/com/br/generation/BlogPessoal/Controller/TemaController.java
+++ BlogPessoal/src/main/java/com/br/generation/BlogPessoal/Controller/TemaController.java
@@ -3,10 +3,15 @@ package com.br.generation.BlogPessoal.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -63,4 +68,47 @@ public class TemaController {
 		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(descricao));		
 	}
 	
+	/**
+	 * Método para postar um tema através da body.
+	 * @author @DreAmVID
+	 * @since 1.0
+	 * @date 04-02-2022
+	 *
+	 */
+	
+	@PostMapping
+		public ResponseEntity<Tema> PostarUmTema(@RequestBody Tema tema) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(repository.save(tema));
+	}
+	
+	/**
+	 * Método para editar um tema através da body.
+	 * @author @DreAmVID
+	 * @since 1.0
+	 * @date 04-02-2022
+	 *
+	 */
+	
+	@PutMapping
+		public ResponseEntity<Tema> EditarUmTema(@RequestBody Tema tema) {
+		return ResponseEntity.ok(repository.save(tema));
+	}
+	
+	/**
+	 * Método para deletar um tema através do ID.
+	 * @author @DreAmVID
+	 * @since 1.0
+	 * @date 04-02-2022
+	 *
+	 */
+	
+	@DeleteMapping("/{id}")
+		public void delete (@PathVariable long id) {
+		repository.deleteById(id);		
+	}
+	
 }
+	
+	
+
