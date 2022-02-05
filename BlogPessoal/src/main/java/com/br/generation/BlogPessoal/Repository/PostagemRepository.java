da3f4f6d51296c2438b9ec732ef6ba6650206781
David <daviidccastro@gmail.com>
2022-02-04 22:27:36 -0300

    @DreAmVID [Criado Repository Postagem e Definido metódos FindAllByTitulo]

---
+++ BlogPessoal/src/main/java/com/br/generation/BlogPessoal/Repository/PostagemRepository.java
@@ -0,0 +1,22 @@
+package com.br.generation.BlogPessoal.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.generation.BlogPessoal.Model.Postagem;
+
+/**
+ * Classe repository utilizada para recuperar métodos do JpaRepository e definir método
+ * FindAllByTitulo
+ * @author @DreAmVID
+ * @since 1.0
+ * @date 04-02-2022
+ *
+ */
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+
+	List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	
+}
